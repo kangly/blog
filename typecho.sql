946f20e569c46c6549986d06ab74c85aa3dd4639
Kangly <371976974@qq.com>
2019-12-19 16:09:31 +0800

    数据库表结构

---
+++ typecho.sql
@@ -0,0 +1,126 @@
+-- --------------------------------------------------------
+-- 主机:                           101.201.233.135
+-- 服务器版本:                        5.7.28 - MySQL Community Server (GPL)
+-- 服务器操作系统:                      Linux
+-- HeidiSQL 版本:                  10.3.0.5771
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- 导出 typecho 的数据库结构
+CREATE DATABASE IF NOT EXISTS `typecho` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `typecho`;
+
+-- 导出  表 typecho.typecho_comments 结构
+CREATE TABLE IF NOT EXISTS `typecho_comments` (
+  `coid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cid` int(10) unsigned DEFAULT '0',
+  `created` int(10) unsigned DEFAULT '0',
+  `author` varchar(200) DEFAULT NULL,
+  `authorId` int(10) unsigned DEFAULT '0',
+  `ownerId` int(10) unsigned DEFAULT '0',
+  `mail` varchar(200) DEFAULT NULL,
+  `url` varchar(200) DEFAULT NULL,
+  `ip` varchar(64) DEFAULT NULL,
+  `agent` varchar(200) DEFAULT NULL,
+  `text` text,
+  `type` varchar(16) DEFAULT 'comment',
+  `status` varchar(16) DEFAULT 'approved',
+  `parent` int(10) unsigned DEFAULT '0',
+  PRIMARY KEY (`coid`),
+  KEY `cid` (`cid`),
+  KEY `created` (`created`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- 导出  表 typecho.typecho_contents 结构
+CREATE TABLE IF NOT EXISTS `typecho_contents` (
+  `cid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) DEFAULT NULL,
+  `slug` varchar(200) DEFAULT NULL,
+  `created` int(10) unsigned DEFAULT '0',
+  `modified` int(10) unsigned DEFAULT '0',
+  `text` longtext,
+  `order` int(10) unsigned DEFAULT '0',
+  `authorId` int(10) unsigned DEFAULT '0',
+  `template` varchar(32) DEFAULT NULL,
+  `type` varchar(16) DEFAULT 'post',
+  `status` varchar(16) DEFAULT 'publish',
+  `password` varchar(32) DEFAULT NULL,
+  `commentsNum` int(10) unsigned DEFAULT '0',
+  `allowComment` char(1) DEFAULT '0',
+  `allowPing` char(1) DEFAULT '0',
+  `allowFeed` char(1) DEFAULT '0',
+  `parent` int(10) unsigned DEFAULT '0',
+  PRIMARY KEY (`cid`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `created` (`created`)
+) ENGINE=MyISAM AUTO_INCREMENT=234 DEFAULT CHARSET=utf8;
+
+-- 导出  表 typecho.typecho_fields 结构
+CREATE TABLE IF NOT EXISTS `typecho_fields` (
+  `cid` int(10) unsigned NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `type` varchar(8) DEFAULT 'str',
+  `str_value` text,
+  `int_value` int(10) DEFAULT '0',
+  `float_value` float DEFAULT '0',
+  PRIMARY KEY (`cid`,`name`),
+  KEY `int_value` (`int_value`),
+  KEY `float_value` (`float_value`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- 导出  表 typecho.typecho_metas 结构
+CREATE TABLE IF NOT EXISTS `typecho_metas` (
+  `mid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) DEFAULT NULL,
+  `slug` varchar(200) DEFAULT NULL,
+  `type` varchar(32) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `count` int(10) unsigned DEFAULT '0',
+  `order` int(10) unsigned DEFAULT '0',
+  `parent` int(10) unsigned DEFAULT '0',
+  PRIMARY KEY (`mid`),
+  KEY `slug` (`slug`)
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+-- 导出  表 typecho.typecho_options 结构
+CREATE TABLE IF NOT EXISTS `typecho_options` (
+  `name` varchar(32) NOT NULL,
+  `user` int(10) unsigned NOT NULL DEFAULT '0',
+  `value` text,
+  PRIMARY KEY (`name`,`user`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- 导出  表 typecho.typecho_relationships 结构
+CREATE TABLE IF NOT EXISTS `typecho_relationships` (
+  `cid` int(10) unsigned NOT NULL,
+  `mid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`cid`,`mid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- 导出  表 typecho.typecho_users 结构
+CREATE TABLE IF NOT EXISTS `typecho_users` (
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `mail` varchar(200) DEFAULT NULL,
+  `url` varchar(200) DEFAULT NULL,
+  `screenName` varchar(32) DEFAULT NULL,
+  `created` int(10) unsigned DEFAULT '0',
+  `activated` int(10) unsigned DEFAULT '0',
+  `logged` int(10) unsigned DEFAULT '0',
+  `group` varchar(16) DEFAULT 'visitor',
+  `authCode` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `mail` (`mail`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
